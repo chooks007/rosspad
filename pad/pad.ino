2734935563355a0a9e5d471be1e1ea147788dbfd
Ross Andrews <randrews@geekfu.org>
2012-11-23 22:41:10 -0600

    Initial commit

---
+++ pad/pad.ino
@@ -0,0 +1,123 @@
+//////////////////////////////////////////////////
+// Config  ///////////////////////////////////////
+//////////////////////////////////////////////////
+
+// Pin with an LED on it
+const int led_pin = 11;
+
+// Number of buttons on the pad
+const int num_buttons = 6;
+
+// Pin for the dpad / mouse toggle
+const int dpad_toggle = 0;
+
+// Pins for analog axes
+const int horizontal = 21;
+const int vertical = 20;
+
+// Limits for analog sticks
+const int h_max = 1010;
+const int h_min = 0;
+
+const int v_max = 1010;
+const int v_min = 0;
+
+// The pins we're using...
+const int pins[] = {
+  1, 2, 13, 14, 15, 4
+};
+
+// And their corresponding letters
+const byte buttons[] = {
+  '1', '2', 'b', 'r', 'y', 'g'
+};
+
+// Dpad buttons, n/s/e/w:
+const byte dpad_buttons[] = {
+  'n', 's', 'e', 'w'
+};
+
+//////////////////////////////////////////////////
+// State stuff ///////////////////////////////////
+//////////////////////////////////////////////////
+
+// Pin states (this must have as many 0s as num_buttons, so if
+// you change that, change this). If it's a 1, the button is pressed
+int pin_states[] = {
+  0, 0, 0, 0, 0, 0
+};
+
+// These, however, there should be only 4 of.
+int dpad_state[] = { 0, 0, 0, 0 }; // n, s, e, w
+
+int analog_pressed = 0;
+int dpad_mode = true;
+
+void setup(){
+  //Setup the pin modes.
+  pinMode( led_pin, OUTPUT );
+
+
+  // Initialize button pins with pull-ups:
+  for(int n = 0; n < num_buttons; n++){
+    pinMode(pins[n], INPUT);
+    digitalWrite(pins[n], HIGH);
+  }
+
+  pinMode(dpad_toggle, INPUT);
+  digitalWrite(dpad_toggle, HIGH);
+
+  pinMode(horizontal, INPUT);
+  pinMode(vertical, INPUT);
+  
+  Keyboard.begin();
+}
+
+void loop(){
+  // Read button states
+  for(int n = 0; n < num_buttons; n++){
+    int new_pin_state = (digitalRead(pins[n]) == LOW);
+    if(pin_states[n] && !new_pin_state) Keyboard.release(buttons[n]);
+    else if(!pin_states[n] && new_pin_state) Keyboard.press(buttons[n]);
+    pin_states[n] = new_pin_state;
+  }
+
+  // Check for analog mode toggle
+  int analog_this_cycle = digitalRead(dpad_toggle) == LOW;
+  if(analog_pressed && ! analog_this_cycle){
+    dpad_mode = !dpad_mode; // Toggle dpad mode
+    Keyboard.releaseAll(); // An emergency out for the keyboard
+  }
+  analog_pressed = analog_this_cycle;
+
+  digitalWrite(led_pin, dpad_mode);
+
+  if(dpad_mode) read_dpad();
+}
+
+void read_dpad(){
+  int h = analogRead(horizontal);
+  int v = analogRead(vertical);
+  
+  // North
+  press_release_dpad(0, v >= v_max);
+  
+  // South
+  press_release_dpad(1, v <= v_min);
+
+  // East
+  press_release_dpad(2, h <= h_min);
+
+  // West
+  press_release_dpad(3, h >= h_max);
+}
+
+void press_release_dpad(int dir, int pressed){
+  if(pressed){
+    if(dpad_state[dir] == 0) Keyboard.press(dpad_buttons[dir]);
+    dpad_state[dir] = 1;
+  } else {
+    if(dpad_state[dir] == 1) Keyboard.release(dpad_buttons[dir]);
+    dpad_state[dir] = 0;
+  }
+}
